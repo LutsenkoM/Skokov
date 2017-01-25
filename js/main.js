1bc3fef154f408833d979ae6ba618f240c3a7b0c
Max <lutsenko.maxim@gmail.com>
2017-01-25 21:00:46 +0200

    1

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $('.multiple-items').slick({
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: false
             }
@@ -35,7 +35,7 @@ $('.multiple-items').slick({
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
         {
